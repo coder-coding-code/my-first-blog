caca03c2a850fd84ee6019e4bd18df3565af5161
admin <kk@gmail.com>
2022-01-14 21:20:46 +0300

    My Django Girls app, first commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import Post
-# Register your models here.
-
+#registering the model to make our model visible
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,14 @@
-from django.conf import settings
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 
-
+#class is  a keyword that indicates we are defining an object.
+#Post is name of our model, always strt class name with CAP
+#models.Model means that Post is a Django model, so Django knows tht it should be saved in database
+#models.charfield = to define text with limited number of characters
+#models.TExtField = long text w/o limit
+#models.DateTimeField = date and time
+#models.foreignkey = link to other model
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -12,6 +18,8 @@ class Post(models.Model):
 
     def publish(self):
         self.published_date = timezone.now()
+        self.save()
     
     def __str__(self):
         return self.title
+    
